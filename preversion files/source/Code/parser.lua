d317f94817260bcc9a62cfa295e64630a52bbe34
EnisDev <90419958+E560000@users.noreply.github.com>
2026-08-04 20:27:50 +0100

    Update parser.lua
    some test

--- preversion files/source/Code/parser.lua
+++ preversion files/source/Code/parser.lua
@@ -16,10 +16,14 @@ function loadMap(difficulty)
 
     for line in contents:gmatch("[^\r\n]+") do
         local orientation, timing = line:match("([^,]+),([^,]+)")
-        table.insert(notes, tonumber(timing), tonumber(orientation))
+        if orientation and timing then
+            table.insert(notes, Note:new(orientation, tonumber(timing)))
+        end
     end
-    for i in notes() do
-      print(notes[i])
+
+    for i, note in ipairs(notes) do
+        print(note.Type, note.Timing)
     end
+
     return notes
 end
